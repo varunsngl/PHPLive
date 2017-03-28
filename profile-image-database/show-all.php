9ab0fda77d3d2e98ee1625487fc36e89f6f4d17f
Varun Kumar <varunsingla33@gmail.com>
2017-03-28 12:05:35 +0530

    Simple php dynamic image upload with mysql
    Simple php dynamic image upload with mysql database

---
+++ profile-image-database/show-all.php
@@ -0,0 +1,42 @@
+<?php 
+	include_once 'config.php';
+
+	$profile_base_path = "http://localhost/PHPLive/profile-image-database/uploads/profile/";
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>All Images</title>
+</head>
+<body>
+	<style type="text/css">
+		.profile-image {
+			height: 250px;
+			width: 250px;
+			object-fit: cover;
+		}
+	</style>
+
+	<?php 
+		$selection_query = "SELECT * FROM `profile_file_upload`";
+		$result = $con->query($selection_query);
+
+		if ($result->num_rows > 0) {
+			while ($row = $result->fetch_assoc()) {?>
+
+				<div style="display: inline-block;">
+					<img class="profile-image" src="<?= $profile_base_path . $row['file_path']  ?>">
+
+					<!--
+						Second Method
+					<?php echo "<img src=\"{$profile_base_path}my-profile.jpg\">" ?> -->
+				</div>
+
+			<?php }
+		}
+	 ?>
+
+	
+</body>
+</html>
---
+++ profile-image-database/upload-form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Upload Image</title>
+</head>
+<body>
+	<form action="upload-receive.php" method="POST" enctype="multipart/form-data">
+		<input type="file" name="profile">
+		<input type="submit" name="submit">
+	</form>
+</body>
+</html>
