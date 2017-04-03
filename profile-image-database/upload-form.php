753615141e410a676ae675d261e2896e869dff78
Rajan Magar <magarrajan786@gmail.com>
2017-04-03 11:19:13 +0530

    comments included

--- profile-image-database/upload-form.php
+++ profile-image-database/upload-form.php
@@ -4,9 +4,12 @@
 	<title>Upload Image</title>
 </head>
 <body>
+	//Creating form to upload file using post method
 	<form action="upload-receive.php" method="POST" enctype="multipart/form-data">
-		<input type="file" name="profile">
-		<input type="submit" name="submit">
+		
+		<input type="file" name="profile">	//To select a file using file type input tag
+		
+		<input type="submit" name="submit">	//On submiting the control will move to upload-recieve.php file as mentioned in form action
 	</form>
 </body>
 </html>
